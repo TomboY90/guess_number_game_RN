3a5a89871a2673e49dbd8ffb98737f767bcc148a
TomboY90 <rjsahdia3@gmail.com>
2020-09-06 10:36:33 +0900

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  extends: '@react-native-community',
+};
---
+++ src/screens/StartGameScreen.jsx
@@ -0,0 +1,97 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+
+import Card from 'components/Card';
+import Input from 'components/Input';
+
+import Colors from '../../constants/colors';
+
+const StartGameScreen = (props) => {
+  const [value, setValue] = useState('');
+
+  const handleInput = (val) => {
+    setValue(val.replace(/[^0-9]/g, '')); // 숫자가 아닌 것들을 '' 빈 스트링으로 변환하겠다
+  };
+
+  const resetButtonHandler = () => {
+    setValue('');
+  };
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}>
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a Game!</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a Number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            autoCapitalize="none"
+            keyboardType="number-pad"
+            maxLength={2}
+            value={value}
+            onChangeText={handleInput}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button
+                title="Reset"
+                onPress={resetButtonHandler}
+                color={Colors.secondary}
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title="Confirm"
+                onPress={() => {}}
+                color={Colors.primary}
+              />
+            </View>
+          </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+  },
+  button: {
+    width: 100,
+  },
+  input: {
+    width: 50,
+    textAlign: 'center',
+  },
+});
+
+export default StartGameScreen;
